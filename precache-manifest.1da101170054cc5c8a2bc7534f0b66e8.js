1ebe0001e661fcbe8c10548082fb013be83a15ff
asif85 <asif85.in@gmail.com>
2020-04-06 17:53:28 +0530

    React Project

---
+++ precache-manifest.1da101170054cc5c8a2bc7534f0b66e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb065d53f72afb8795c4f1f1149579b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc9e06fda99be32156fd",
+    "url": "/static/css/main.6727ba5c.chunk.css"
+  },
+  {
+    "revision": "af3970788b88c0341fb2",
+    "url": "/static/js/2.92a7cceb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.92a7cceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9e06fda99be32156fd",
+    "url": "/static/js/main.0107a043.chunk.js"
+  },
+  {
+    "revision": "86b71b85369e2b1eb5db",
+    "url": "/static/js/runtime-main.4128e1b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
